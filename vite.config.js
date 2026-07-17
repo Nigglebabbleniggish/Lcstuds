1bdd3fb6080dc275863ce4e8073f0d06852e9bc2
Nigglebabbleniggish <kurpiu18@gmail.com>
2026-07-17 15:48:15 +0200

    Remove base path to fix 404 errors

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/affiliate-dashboard/', // Replace with your GitHub repo name
   server: {
     port: 3000,
     host: true
