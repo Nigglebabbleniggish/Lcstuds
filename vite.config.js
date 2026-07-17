8040688cb66333f04fbb69b76de381fa92aab480
Nigglebabbleniggish <kurpiu18@gmail.com>
2026-07-17 16:10:31 +0200

    Fix vite config for gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Lcstuds/',
   server: {
     port: 3000,
     host: true
-  },
-  build: {
-    outDir: 'docs'
   }
 })
